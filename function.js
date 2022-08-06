5c4a928ca164a8006dbfa7b9d14dc8916a12e079
rasel726 <md.rasel2726@gmail.com>
2022-08-06 16:47:17 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,15 @@
+// function decleration
+function startFan(){
+    console.log('Stant Up');
+    console.log('Walk towards the switch');
+    console.log('Press the switch');
+}
+
+// call ba function
+startFan();
+console.log('Waking up in the morning');
+startFan();
+console.log('Eating lunch');
+startFan();
+console.log('Watching JS tutorial');
+startFan();
