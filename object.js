da6881f754bf20ed8792c68bc643eb537f114f0d
rasel726 <md.rasel2726@gmail.com>
2022-08-06 21:03:48 +0600

    object file added

---
+++ object.js
@@ -0,0 +1,23 @@
+var student = {
+    id: 115,
+    name: 'Solaiman Khan',
+    class: 9,
+    marks: 98
+}
+
+var mobile = {
+    brand: 'Samsung',
+    price: 19000,
+    storage: '16gb',
+    camera: '7MP'
+}
+
+var myComputer = {
+    brand: 'lenevo',
+    price: 39000,
+    color: 'Silver',
+    processor: 'i7'
+}
+
+console.log(myComputer.price);
+console.log(mobile.brand);
